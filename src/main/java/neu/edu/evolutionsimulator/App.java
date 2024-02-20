e787b849d7fb36df468efec3da683bae7a7fd915
Xijing Zhang <zhangxijing97@gmail.com>
2024-02-20 11:19:42 -0800

    Initialization

---
+++ src/main/java/neu/edu/evolutionsimulator/App.java
@@ -0,0 +1,11 @@
+package neu.edu.evolutionsimulator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
