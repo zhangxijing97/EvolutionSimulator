3c6796537df5d02707aeeff03ff699c4759b4d2d
Xijing Zhang <zhangxijing97@gmail.com>
2024-02-20 12:12:27 -0800

    Update

--- src/main/java/neu/edu/evolutionsimulator/App.java
+++ src/main/java/neu/edu/evolutionsimulator/App.java
@@ -1,11 +1,31 @@
 package neu.edu.evolutionsimulator;
 
-/**
- * Hello world!
- *
- */
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Creating instance of JFrame
+        JFrame frame = new JFrame();
+
+        // Creating instance of JButton
+        JButton button = new JButton(" GFG WebSite Click");
+
+        // x axis, y axis, width, height
+        button.setBounds(150, 200, 220, 50);
+
+        // adding button in JFrame
+        frame.add(button);
+
+        // 400 width and 500 height
+        frame.setSize(500, 600);
+
+        // using no layout managers
+        frame.setLayout(null);
+
+        // making the frame visible
+        frame.setVisible(true);
+
     }
 }
